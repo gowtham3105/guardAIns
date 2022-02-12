fb1cb7dba5fca14173d0cfdb51014b254079ae34
gowtham3105 <gowthamsai.workspace@gmail.com>
2022-02-13 04:49:17 +0530

    Small Changes in Clue.py, Beast.py
    Signed-off-by: gowtham3105 <gowthamsai.workspace@gmail.com>

--- Cells/Beast.py
+++ Cells/Beast.py
@@ -1,21 +1,22 @@
-from functools import reduce
+from Cells.Cell import Cell
+from Cells.Clue import Clue
 from Feedback import Feedback
 from Player import Player
-from .Clue import Clue
-from InfinityStone import InfinityStone
+
 
 class Beast(Clue):
-    def __init__(self,player1:Player,player2:Player ,infinityStone: InfinityStone):
-        super().__init__()
+    def __init__(self, cell: Cell):
+        super().__init__(cell, Cell.Beast)
         self.__is_alive = True
 
-    def update_health(self, player:Player): # call this in update rounds every round for each player and it returns the clue if available
+    def update_health(self,
+                      player: Player):  # call this in update rounds every round for each player and it returns the clue if available
         if self.__is_alive:
             if len(self.__rounds_left) > 0:
                 self.__rounds_left -= 1
             guardian = self.get_guardians_present()[0]
             if guardian.belongs_to_player == player:
-                reduce_health =  guardian.set_health(guardian.get_health() - self.__damage)
+                reduce_health = guardian.set_health(guardian.get_health() - self.__damage)
                 if reduce_health != None:
                     return reduce_health
                 else:
--- Cells/Clue.py
+++ Cells/Clue.py
@@ -1,26 +1,31 @@
 import random
 
+from Cells.Cell import Cell
 from InfinityStone import InfinityStone
 from Player import Player
-
-from ..Cell import Cell
 
 type_of_clue = ["enemy_seen", "node_Optimal_path"]
 
 
 class Clue(Cell):
-    def __init__(self):
-        super().__init__()
-        self.type = random.choice(type_of_clue) # equal probability
+    def __init__(self, cell: Cell, cell_type=None):
+        if not cell_type:
+            super().__init__(cell.get_coordinates(), cell.get_guardians_present(), cell.get_neighbour_cells(),
+                             Cell.Clue)
+        else:
+            super().__init__(cell.get_coordinates(), cell.get_guardians_present(), cell.get_neighbour_cells(),
+                             cell_type)
+        self.type = random.choice(type_of_clue)  # equal probability
         self.is_clue_active = False
-    def enemy_location(self,opponentPlayer:Player):
+
+    def enemy_location(self, opponentPlayer: Player):
         return_dict = {}
-        for troop_name,troop in opponentPlayer.guardians().items():
+        for troop_name, troop in opponentPlayer.guardians().items():
             if troop.is_alive:
-                return_dict[troop_name] =  str(troop.get_coordinates())
+                return_dict[troop_name] = str(troop.get_coordinates())
         return {"enemy_locations": return_dict}
 
-    def get_best_direction(self,infinityStone:InfinityStone):
+    def get_best_direction(self, infinityStone: InfinityStone):
         direcionSlope = (self.get_coordinates()[1]-infinityStone.get_coordinates()[1])/(self.get_coordinates()[0]-infinityStone.get_coordinates()[0]) # TODO: check order
         return {"infinity_stone_direction":direcionSlope}
     def get_clue(self,opponentPlayer:Player,infinityStone:InfinityStone, player:Player):
